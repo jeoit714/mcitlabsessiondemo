50c716a06e522e73f753f8d8e3f272a717e48947
jeoit714 <168040201+jeoit714@users.noreply.github.com>
2024-05-05 20:03:15 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,3 +10,8 @@ value=var.check
 output "sporthiverslist"{
 value=[for sport in local.sporthiverslist:sport]
 }
+/*
+for 1st element in -->inside the list variable winterlistOfSports--->"icehockey"1st element
+for 2nd element in -->inside the list variable winterlistOfSports--->"snowboarding" 2nd element
+for 3rd element in -->inside the list variable winterlistOfSports--->"iceskating" 3rd element
+*/
